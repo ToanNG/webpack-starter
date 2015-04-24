aba45b4d1c4fc8b703db7dfc12aede577c9cbb80
Toan Nguyen Gia <toan.nguyen@2359media.com>
2015-04-24 15:46:59 +0700

    remove bootstrap sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 var path = require('path');
 var webpack = require('webpack');
 var bourbon = require('node-bourbon').includePaths;
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: [
-    'bootstrap-sass!./bootstrap-sass.config.js',
     './app/scripts/index'
   ],
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
     publicPath: '/assets/',
     contentBase: 'public/'
   },
+  plugins: [
+    new ExtractTextPlugin('styles.css')
+  ],
   resolve: {
     modulesDirectories: ['web_modules', 'node_modules', 'app/scripts', 'app/styles'],
     extensions: ['', '.js', '.jsx']
@@ -26,7 +29,8 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        loader: 'style!css!sass?includePaths[]=' + bourbon
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader?includePaths[]=' + bourbon + '&' +
+          'includePaths[]=' + path.resolve(__dirname, './node_modules/bootstrap-sass/assets/stylesheets'))
       },
 
       // **IMPORTANT** This is needed so that each bootstrap js file required by
@@ -35,11 +39,11 @@ module.exports = {
 
       // Needed for the css-loader when [bootstrap-webpack](https://github.com/bline/bootstrap-webpack)
       // loads bootstrap's css.
-      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=application/font-woff" },
-      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=application/font-woff" },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=application/octet-stream" },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=image/svg+xml" }
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&minetype=application/font-woff' },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&minetype=application/font-woff' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&minetype=application/octet-stream' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&minetype=image/svg+xml' }
     ]
   }
 };
